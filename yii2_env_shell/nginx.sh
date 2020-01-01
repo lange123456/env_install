ea5acf73a5546f9e9240c1bd54c1c405e2fbd5f8
lange123456 <Lange@gmail.com>
2020-01-01 15:45:30 +0800

    nginx 修改

--- yii2_env_shell/nginx.sh
+++ yii2_env_shell/nginx.sh
@@ -15,8 +15,6 @@ cd /data/src/tengine-2.2.2
 --with-openssl= \
 --with-http_gzip_static_module \
 --with-file-aio \
---with-http_concat_module \
---with-http_footer_filter_module=shared \
 --with-http_stub_status_module \
 --with-http_sub_module \
 --with-http_realip_module \
